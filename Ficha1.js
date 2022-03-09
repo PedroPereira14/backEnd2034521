35ef17c9fa4166c315e0fd79275bb9428be9237e
Pedro António <pedroantonioxd@gmail.com>
2022-03-09 17:42:42 +0000

    Create Ficha1.js

---
+++ Ficha1.js
@@ -0,0 +1,88 @@
+//nota teorica 30% e nota pratica 70%
+//se for <9 é reprovado
+
+//ex 5
+
+/*function finalgrade(practice, theoretical){
+    var final = practice * 0.7 + theoretical * 0.3;
+
+    console.log("A sua nota foi " + final )
+
+    if (final < 9.5) {
+    console.log("Reprovado");
+    }
+    else  {
+    console.log("Aprovado");
+    }
+}
+
+finalgrade(12,13);*/
+
+/*ex 6
+function month(NumMonth){
+    if (NumMonth == 1){
+    console.log("Janeiro")
+    }
+    else if (NumMonth == 2){
+    console.log("Fevereiro")
+    }
+    else if (NumMonth == 3){
+    console.log("Março")
+    }
+    else if (NumMonth == 4){
+    console.log("Abril")    
+    }
+    else if (NumMonth == 5){
+    console.log("Maio")
+    }
+    else if (NumMonth == 6){
+    console.log("Junho")
+    }
+    else if (NumMonth == 7){
+    console.log("Julho")
+    }
+    else if (NumMonth == 8){
+    console.log("Agosto")
+    }
+    else if (NumMonth == 9){
+    console.log("Setembro")
+    }
+    else if (NumMonth == 10){
+    console.log("Outubro")
+    }
+    else if (NumMonth == 11){
+    console.log("Novembro")
+    }
+    else if (NumMonth == 12){
+    console.log("Dezembro")
+    }
+    else {
+        console.log("Não existe")
+    }
+}
+month(13)*/
+
+//ex7
+/*function result(sum1, op, sum2){
+    if (op == '+') {
+        console.log("A soma é igual a ", sum1 + sum2);
+    }
+    else if (op == '-') {
+        console.log("A subtração foi ", sum1 - sum2);
+    }
+    else if (op == '*') {
+        console.log("A subtração foi ", sum1 * sum2);
+    }
+    else if (op == '/') {
+        console.log("A divisão foi ", sum1 / sum2);
+    }
+}
+ result(12, '+', 13);
+ result(12, '-', 13);
+ result(12, '*', 13);
+ result(12, '/', 13);*/
+
+ //ex 8
+
+ 
+
