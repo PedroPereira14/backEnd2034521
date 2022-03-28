ffcf62443197cc56bd7508f52da397682849cdb5
Pedro António <pedroantonioxd@gmail.com>
2022-03-28 17:47:01 +0100

    s

--- Ficha3/app.js
+++ Ficha3/app.js
@@ -1,12 +1,21 @@
 function started(){
-
+    console.log("Started Download");
 };
-function update(){
-
+function update(progress){
+        console.log(progress + "% of Download");
 };
 function completed(){
-
+    console.log("Download Complete.");
 };
 
+function performDownload(started_fn, update_fn, completed_fn){
+    started_fn();
+    for (let i = 0; i <= 100; i++){
+        update_fn(i);
+    }
+    completed_fn();
+    
+
+};
 
 performDownload(started, update, completed);
